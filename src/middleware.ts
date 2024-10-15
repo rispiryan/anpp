9abbf4dc8f87bc7abc27968f2950287c4c1a48b9
Levon <levon@nodoteam.com>
2024-10-16 01:18:34 +0400

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-// import createMiddleware from 'next-intl/middleware';
-//
-// import { routing } from './i18n/routing';
-//
-// export default createMiddleware(routing);
-//
-// export const config = {
-//   // Match only internationalized pathnames
-//   matcher: ['/', '/(ar|en|ru)/:path*'],
-// };
+import createMiddleware from 'next-intl/middleware';
 
-import createIntlMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-export default createIntlMiddleware({
-  locales: ['ar', 'en', 'ru'], // Add your supported locales
-  defaultLocale: 'ar',
-});
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'], // Adjust the paths where the middleware should run
+  // Match only internationalized pathnames
+  matcher: ['/', '/(ar|en|ru)/:path*'],
 };
+
+// import createIntlMiddleware from 'next-intl/middleware';
+//
+// export default createIntlMiddleware({
+//   locales: ['ar', 'en', 'ru'], // Add your supported locales
+//   defaultLocale: 'ar',
+// });
+//
+// export const config = {
+//   matcher: ['/((?!api|_next|.*\\..*).*)'], // Adjust the paths where the middleware should run
+// };

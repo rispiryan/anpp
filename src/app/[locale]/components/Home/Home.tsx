2b851fbf92ba634c35cf4707c91db70655490eeb
Razmik <razmik@Razmiks-MacBook-Pro.local>
2024-10-24 19:14:25 +0400

    feat: added paralax

--- src/app/[locale]/components/Home/Home.tsx
+++ src/app/[locale]/components/Home/Home.tsx
@@ -1,30 +1,13 @@
 import { useTranslations } from 'next-intl';
-import Link from 'next/link';
-import cn from 'classnames';
 
 import HomeNews from '@/app/[locale]/components/Home/HomeNews';
 
-import { routes } from '@/constants/routes';
+import HomeSections from './HomeSections';
 
 import styles from './Home.module.scss';
 interface IHome {
   news: INews[];
 }
-
-const links = [
-  { href: routes.safety, title: 'safety' },
-  { href: routes.vacancies, title: 'vacancies' },
-  { href: routes.production, title: 'production' },
-  { href: routes.electricalWorkshop, title: 'electricalWorkshop' },
-  { href: routes.structure, title: 'structure' },
-  { href: routes.cooperation, title: 'cooperation' },
-  { href: routes.report, title: 'report' },
-  { href: routes.events, title: 'events' },
-  { href: routes.history, title: 'history' },
-  { href: routes.zachFactory, title: 'zachFactory' },
-  { href: routes.reactorFactory, title: 'reactorFactory' },
-  { href: routes.turbineFactory, title: 'turbineFactory' },
-];
 
 const Home = ({ news }: IHome) => {
   const t = useTranslations();
@@ -44,15 +27,7 @@ const Home = ({ news }: IHome) => {
               <HomeNews key={el.id} news={el} />
             ))}
           </div>
-          <div className={styles.imagesWrap}>
-            {links.map((el) => (
-              <Link href={el.href} key={el.title}>
-                <div
-                  className={cn(styles.image, { [styles[el.title]]: el.title })}
-                />
-              </Link>
-            ))}
-          </div>
+          <HomeSections />
         </div>
       </div>
     </div>
---
+++ src/app/[locale]/components/Home/HomeSections/HomeSections.tsx
@@ -0,0 +1,61 @@
+'use client';
+import { useEffect, useRef } from 'react';
+
+import Link from 'next/link';
+import cn from 'classnames';
+
+import { routes } from '@/constants/routes';
+
+import styles from './HomeSections.module.css';
+
+const links = [
+  { href: routes.safety, title: 'safety' },
+  { href: routes.vacancies, title: 'vacancies' },
+  { href: routes.production, title: 'production' },
+  { href: routes.electricalWorkshop, title: 'electricalWorkshop' },
+  { href: routes.structure, title: 'structure' },
+  { href: routes.cooperation, title: 'cooperation' },
+  { href: routes.report, title: 'report' },
+  { href: routes.events, title: 'events' },
+  { href: routes.history, title: 'history' },
+  { href: routes.zachFactory, title: 'zachFactory' },
+  { href: routes.reactorFactory, title: 'reactorFactory' },
+  { href: routes.turbineFactory, title: 'turbineFactory' },
+];
+
+const HomeSections = () => {
+  const sectionsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (sectionsRef.current) {
+        const docScrollTop = document.documentElement.scrollTop;
+        const docScrollHeight =
+          document.documentElement.scrollHeight -
+          document.documentElement.clientHeight;
+
+        // Get the custom div scroll information
+        const customDivScrollHeight =
+          sectionsRef.current.scrollHeight - sectionsRef.current.clientHeight;
+
+        // Calculate the scroll ratio
+        const scrollRatio = docScrollTop / docScrollHeight;
+
+        // Apply the same scroll ratio to the custom div
+        sectionsRef.current.scrollTop = scrollRatio * customDivScrollHeight;
+      }
+    });
+  }, []);
+
+  return (
+    <div className={styles.imagesWrap} ref={sectionsRef}>
+      {links.map((el) => (
+        <Link href={el.href} key={el.title}>
+          <div className={cn(styles.image, { [styles[el.title]]: el.title })} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default HomeSections;
